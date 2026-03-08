9d0dedcd250741f49cea0ef7d9de0f31cee34386
kimyu4606 <kimyu4606@gmail.com>
2026-03-08 14:43:36 +0000

    added phone number change xD

--- src/components/SuccessPage.tsx
+++ src/components/SuccessPage.tsx
@@ -19,7 +19,7 @@ function SuccessPage({ bulkFormsLength, bulkPaymentMethod, onFinishRegistration 
         <div className="online-payment-card">
           <h3 className="online-payment-title">Online Payment Instructions</h3>
           <p className="helper-text">Total amount is <strong className="total-amount">₱{bulkFormsLength * 600}</strong> according to the delegates you registered.</p>
-          <p className="helper-text">Please send your payment to GCash number <strong className="gcash-number">09619605811</strong>.</p>
+          <p className="helper-text">Please send your payment to GCash number <strong className="gcash-number">09958367243</strong>.</p>
           <p className="helper-text">After paying, send a message on Messenger to <strong className="messenger-name">Aldo Yu</strong> with your church name and number of delegates.</p>
         </div>
 
